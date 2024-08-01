3b4fc9502b341c144627de9db3c09f0d3ec358bc
Anna Soboleva <barbi-bar@mail.ru>
2024-08-01 18:10:08 +0300

    fix: Update index.js

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ module.exports = function toReadable (number) {
   let below_twenty = ["ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"];
   let below_hundred = ["twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"];
   if (number < 10) {
-    return single_number[number] + " ";
+    return single_number[number];
   } else if (number > 9 && number < 20) {
-      return below_twenty [number - 10] + " ";
+      return below_twenty [number - 10];
   } else if (number > 19 && number < 100) {
       let second_number = toReadable (number % 10);
       return below_hundred[(number - (number % 10)) / 10 -2] + " " + second_number;
